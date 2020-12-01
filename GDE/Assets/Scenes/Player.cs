f4096d39723b6aac1cf7de75647dc31f1287fe15
Srivathsan01 <srivathsanbaskaran1@gmail.com>
2020-12-02 01:18:41 +0530

    Added Animations
    Added locomotion with bugs

---
+++ GDE/Assets/Scenes/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var v = Input.GetAxis("Vertical");
+        var h = Input.GetAxis("Horizontal");
+        anim.SetFloat("speed",v);
+        anim.SetFloat("turningspeed",h);
+
+
+    }
+}
