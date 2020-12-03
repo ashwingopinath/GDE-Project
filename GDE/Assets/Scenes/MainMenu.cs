a533fb692ed29b80aaa96d05986c7356d0f63c0c
Srivathsan01 <srivathsanbaskaran1@gmail.com>
2020-12-03 21:55:48 +0530

    Main Menu Screen
    Main Menu Screen

---
+++ GDE/Assets/Scenes/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    public void GoToSettingsMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SettingsMenu");
+
+    }
+    public void GoToMainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
