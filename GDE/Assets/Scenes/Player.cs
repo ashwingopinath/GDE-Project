a881f3a68e9e72f53e986d47410f2de2df1085da
Sambhav Solanki <sambhav.solanki@research.iiit.ac.in>
2020-12-03 22:46:28 +0530

    Added run and refined jump animation

--- GDE/Assets/Scenes/Player.cs
+++ GDE/Assets/Scenes/Player.cs
@@ -17,7 +17,13 @@ public class Player : MonoBehaviour
     void Update()
     {
         var v = Input.GetAxis("Vertical");
-        anim.SetFloat("speed",v);
+        if(Input.GetKey(KeyCode.LeftShift)){
+            anim.SetFloat("speed",Math.Min(anim.GetFloat("speed")+0.05f,2*v));
+        }
+        else
+        {
+            anim.SetFloat("speed",Math.Max(anim.GetFloat("speed")-0.05f,v));
+        }
         if(Input.GetKey(KeyCode.A))
             anim.SetFloat("turn",Math.Min(anim.GetFloat("turn")+0.05f,0.5f));
         else if(Input.GetKey(KeyCode.D))
@@ -29,13 +35,15 @@ public class Player : MonoBehaviour
             else if(anim.GetFloat("turn") < 0.0f){
                 anim.SetFloat("turn",Math.Min(anim.GetFloat("turn") + 0.05f,0.0f));
             }
-        }
-
+            
+        }        
         if(Input.GetKeyDown(KeyCode.Space)){
-            anim.SetBool("Jump",true);
-            GetComponent<Rigidbody>().AddForce(Vector3.forward * 10, ForceMode.VelocityChange);
+            GetComponent<Rigidbody>().AddForce(Vector3.up,ForceMode.VelocityChange);
+            anim.SetBool("Grounded",true);
         }
-        else
-            anim.SetBool("Jump",false);
+        if(GetComponent<Rigidbody>().velocity.y > 0.9)
+            anim.SetBool("Grounded",false);
+        anim.SetFloat("Velocity",GetComponent<Rigidbody>().velocity.y);
+
     }
 }
