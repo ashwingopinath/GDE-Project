2bfaaff19eab04ccd6a67dc34aaa3b992369b1cc
Sambhav Solanki <sambhav.solanki@research.iiit.ac.in>
2020-12-05 10:45:49 +0530

    Forked the endGame script

---
+++ GDE/Assets/EndGameLose.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    
+    public void OnTriggerEnter(Collider coll)
+    {   
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
+    }
+}
